a770ab0f77ce75b6f7e1c2cc4a6cb2e487700dca
RAKHI <RAKHI@DESKTOP-6LDC70F>
2023-03-06 06:20:51 -0800

    first commit

---
+++ my_maventest/src/main/java/my_maventest/Hello.java
@@ -0,0 +1,20 @@
+package my_maventest;
+
+public class Hello {
+
+	public void display()
+		{
+			System.out.println("Hello World");
+		}
+
+	public static void main(String[] args)
+		{
+		
+			Hello obj=new Hello();
+			obj.display();
+			
+				}
+
+
+
+}
